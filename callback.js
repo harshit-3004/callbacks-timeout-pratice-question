33398f1ba1adaf8b372d8452966f26f03477a0d8
harshit-3004 <h.chaubisa30@gmail.com>
2022-03-31 15:45:04 +0530

    callbacks

---
+++ callback.js
@@ -0,0 +1,26 @@
+var items=[
+    {
+        stuName : "Harshit",
+        rollNo : 11,
+        course : "FSD",
+    },
+    {
+        stuName : "Rohan",
+        rollNo : 12,
+        course : "CAD"
+    },
+    ]
+    
+
+
+function getItems(){
+    items.forEach((student) =>{
+        console.log(student);
+    })
+}
+
+function addItem(newItem,callback){
+    items.push(newItem);
+}
+
+addItem({stuName : "Chirag", rollNo : 13 , course : "CAM"},getItems());
